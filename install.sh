b21ce6edf61bf09264d405274d753ca8072475af
David <nasdevelopers8@gmail.com>
2026-07-04 21:31:17 +0700

    Set up Go environment in install.sh
    Added Go environment variables and installation steps.

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ export ANDROID_HOME="${HOME}/Android/Sdk"
 export ANDROID_SDK_ROOT="${ANDROID_HOME}"
 export ANDROID_NDK_HOME="${HOME}/Android/Sdk/ndk/android-ndk-r29"
 export ANDROID_NDK_TOOLCHAINS="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-aarch64"
-export PATH="${PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin"
+export GOROOT="${HOME}/go"
+export GOPATH="${HOME}/go/packages"
+export PATH="${PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin:${GOROOT}/bin:${GOPATH}/bin"
 EOF
 else
 # shellcheck disable=2016
@@ -20,7 +22,9 @@ export ANDROID_HOME="${HOME}/Android/Sdk"
 export ANDROID_SDK_ROOT="${ANDROID_HOME}"
 export ANDROID_NDK_HOME="${HOME}/Android/Sdk/ndk/android-ndk-r29"
 export ANDROID_NDK_TOOLCHAINS="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-aarch64"
-export PATH="${PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin"
+export GOROOT="${HOME}/go"
+export GOPATH="${HOME}/go/packages"
+export PATH="${PATH}:${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin:${GOROOT}/bin:${GOPATH}/bin"
 '
 fi
 export JAVA_HOME="$PREFIX/lib/jvm/java-21-openjdk"
@@ -28,7 +32,9 @@ export ANDROID_HOME="${HOME}/Android/Sdk"
 export ANDROID_SDK_ROOT="${ANDROID_HOME}"
 export ANDROID_NDK_HOME="${HOME}/Android/Sdk/ndk/android-ndk-r29"
 export ANDROID_NDK_TOOLCHAINS="${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-aarch64"
-export PATH="${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin:${PATH}"
+export GOROOT="${HOME}/go"
+export GOPATH="${HOME}/go/packages"
+export PATH="${ANDROID_HOME}/cmdline-tools/latest/bin:${ANDROID_HOME}/platform-tools:${ANDROID_NDK_HOME}:${ANDROID_NDK_TOOLCHAINS}/bin:${GOROOT}/bin:${GOPATH}/bin:${PATH}"
 pkg update
 pkg install aapt aapt2 aidl android-tools apksigner d8 jq openjdk-21 p7zip unzip wget -y
 wget --tries=100 --retry-connrefused --waitretry=5 -O studio.html https://developer.android.com/studio
@@ -58,6 +64,9 @@ cd ~ || exit
 wget --tries=100 --retry-connrefused --waitretry=5 https://github.com/lzhiyong/termux-ndk/releases/download/android-ndk/android-ndk-r29-aarch64.7z
 7z x android-ndk-r29-aarch64.7z -o"${HOME}/Android/Sdk/ndk"
 rm android-ndk-r29-aarch64.7z*
+# Install Go (Golang)
+pkg install golang -y
+mkdir -p "${GOPATH}"
 mkdir -p ~/.gradle
 cat > ~/.gradle/gradle.properties << 'EOF'
 android.aapt2FromMavenOverride=/data/data/com.termux/files/usr/bin/aapt2
